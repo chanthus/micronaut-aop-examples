091a7db825e5fcaf08c08d39e2f74cb20443b3f8
chanthu <chanthu@trunkplatform.com>
2020-04-05 12:33:52 +1000

    Removed unused annotations

--- src/main/java/micronaut/aop/advice/around/annotations/exceptionReMap/ReMapNotFoundException.java
+++ src/main/java/micronaut/aop/advice/around/annotations/exceptionReMap/ReMapNotFoundException.java
@@ -3,14 +3,12 @@ package micronaut.aop.advice.around.annotations.exceptionReMap;
 import io.micronaut.aop.Around;
 import io.micronaut.context.annotation.Type;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Documented
 @Retention(RUNTIME)
 @Target({ElementType.METHOD})
 @Around
--- src/main/java/micronaut/aop/advice/methodAdapter/MessageListener.java
+++ src/main/java/micronaut/aop/advice/methodAdapter/MessageListener.java
@@ -2,14 +2,12 @@ package micronaut.aop.advice.methodAdapter;
 
 import io.micronaut.aop.Adapter;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Documented
 @Retention(RUNTIME)
 @Target({ElementType.METHOD})
 @Adapter(ApplicationMessageListener.class)
